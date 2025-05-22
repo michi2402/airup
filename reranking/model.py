4bfa261b57fadfcc3c33414cea2b85d18533bc23
suvi- <suvi-arck@hotmail.com>
2025-05-22 18:58:36 +0200

    added comments to model.py

--- reranking/model.py
+++ reranking/model.py
@@ -5,12 +5,14 @@ from reranking.config import DATA_PATH, RERANKER_PATH
 from reranking.data_preprocessing import get_testing_data
 
 def load_model(model_dir = RERANKER_PATH):
+    """Loads a pretrained tokenizer and sequence classification model from a directory."""
     tokenizer = AutoTokenizer.from_pretrained(model_dir)
     model = AutoModelForSequenceClassification.from_pretrained(model_dir)
-    model.eval()  # set to evaluation mode ??
+    model.eval()
     return tokenizer, model
 
 def rerank(tokenizer, model, question, snippets, docs, q_ids, snippet_data):
+    """Tokenize question-snippet pairs, get relevance scores from model, and return snippets sorted by score"""
     inputs = tokenizer(
         text=[question] * len(snippets),
         text_pair=snippets,
@@ -23,10 +25,11 @@ def rerank(tokenizer, model, question, snippets, docs, q_ids, snippet_data):
         scores = model(**inputs).logits.squeeze(-1)
 
     #rerank by score
-    reranked_docs = sorted(zip(snippets, docs, scores.tolist(), q_ids, snippet_data), key=lambda x: x[1], reverse=True)
-    return reranked_docs
+    reranked_snippets = sorted(zip(snippets, docs, scores.tolist(), q_ids, snippet_data), key=lambda x: x[1], reverse=True)
+    return reranked_snippets
 
 def rerank_docs(tokenizer, model, question, doc_links,docs , q_ids):
+    """Tokenize question-doc pairs, get relevance scores from model, and return docs sorted by score"""
     inputs = tokenizer(
         text=[question] * len(docs),
         text_pair=docs,
